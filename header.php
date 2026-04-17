777d9a7bd97b970d1410922b309d0fffd0766144
Alok Kumar <alokk986963@gmail.com>
2026-04-18 00:25:06 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from maha-cv-resume-bootstrap.vercel.app/home-two.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 15 Mar 2026 14:44:14 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <title>Alok - Personal CV/Resume  </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Maha Personal cv/resume template for professional and personal website." />
+    <meta name="keywords"
+        content="creative, cv, designer,  online cv, online resume, powerful portfolio, professional, professional resume, responsive, resume, vcard " />
+    <meta name="developer" content="Md. Siful Islam">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- FAV AND ICONS   -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="assets/images/apple-icon.png">
+    <link rel="shortcut icon" sizes="72x72" href="assets/images/apple-icon-72x72.png">
+    <link rel="shortcut icon" sizes="114x114" href="assets/images/apple-icon-114x114.png">
+
+    <!-- Google Font-->
+    <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/icons/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/plugins/css/bootstrap.min.css">
+    <!-- Animate CSS-->
+    <link rel="stylesheet" href="assets/plugins/css/animate.css">
+    <!-- Owl Carousel CSS-->
+    <link rel="stylesheet" href="assets/plugins/css/owl.css">
+    <!-- Fancybox-->
+    <link rel="stylesheet" href="assets/plugins/css/jquery.fancybox.min.css">
+
+    <!-- Custom CSS-->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!-- Colors -->
+    <link rel="alternate stylesheet" href="assets/css/colors/blue.css" title="blue">
+    <link rel="stylesheet" href="assets/css/colors/defauld.css" title="defauld">
+    <link rel="alternate stylesheet" href="assets/css/colors/green.css" title="green">
+    <link rel="alternate stylesheet" href="assets/css/colors/blue-munsell.css" title="blue-munsell">
+    <link rel="alternate stylesheet" href="assets/css/colors/orange.css" title="orange">
+    <link rel="alternate stylesheet" href="assets/css/colors/purple.css" title="purple">
+    <link rel="alternate stylesheet" href="assets/css/colors/slate.css" title="slate">
+    <link rel="alternate stylesheet" href="assets/css/colors/yellow.css" title="yellow">
+
+</head>
+
+<body class="dark-vertion black-bg">
+
+    
+
+    <!--
+        ===================
+           NAVIGATION
+        ===================
+        -->
+    <header class="black-bg mh-header mh-fixed-nav nav-scroll mh-xs-mobile-nav" id="mh-header">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <nav class="navbar navbar-expand-lg mh-nav nav-btn">
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/images/fedalon_logo1.png" alt="maha" class="img-fluid">
+                        <!-- <h2>Maha</h2> -->
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-0 ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#mh-home">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mh-about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mh-skills">Skills</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mh-experience">Experiences</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mh-portfolio">Portfolio</a>
+                            </li>
+                            <!-------<li class="nav-item">
+                                <a class="nav-link" href="#mh-pricing">Pricing</a>
+                            </li>---->
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mh-blog">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#mh-contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
